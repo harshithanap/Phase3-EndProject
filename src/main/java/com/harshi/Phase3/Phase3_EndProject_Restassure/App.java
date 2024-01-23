ee1fc50a0aad56039adcb6d62d5d69947e057f81
harshithanap <ashwathharshitha0@gmail.com>
2024-01-23 11:53:10 +0530

    project set up for rest assure

---
+++ src/main/java/com/harshi/Phase3/Phase3_EndProject_Restassure/App.java
@@ -0,0 +1,13 @@
+package com.harshi.Phase3.Phase3_EndProject_Restassure;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
